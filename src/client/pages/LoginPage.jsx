6e4508a4f051ff9558bcde8762ac404eac1b1e6c
SiriusbluE23 <2807rut@gmail.com>
2026-02-19 13:33:22 +0530

    third commit

--- src/client/pages/LoginPage.jsx
+++ src/client/pages/LoginPage.jsx
@@ -13,7 +13,7 @@ function LoginPage({ onLogin }) {
     setIsError(false)
 
     try {
-      const res = await fetch('/api/login', {
+      const res = await fetch('https://appraisalcalculationbackend.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
